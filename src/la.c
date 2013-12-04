a90a6c5088519b0780da285333c1d4aef0b5b4aa
Michael Hartmann <github@speicherleck.de>
2013-12-04 08:50:36 +0100

    * add function la_matrix_info that writes various information about a matrix to   a stream

--- src/la.c
+++ src/la.c
@@ -26,3 +26,29 @@ double la_matrix_trace(gsl_matrix *A)
 
     return trace;
 }
+
+void la_matrix_info(FILE *stream, gsl_matrix *M)
+{
+    int i,j;
+    int zeros = 0, nans = 0, infs = 0;
+    double min, max;
+    min = max = fabs(gsl_matrix_get(M, 0, 0));
+
+    for(i = 0; i < M->size1; i++)
+        for(j = 0; j < M->size2; j++)
+         {
+             double elem = fabs(gsl_matrix_get(M, i,j));
+             if(isnan(elem))
+                 nans++;
+             if(isinf(elem))
+                 infs++;
+             if(elem == 0)
+                 zeros++;
+             min = MIN(min, elem);
+             max = MAX(max, elem);
+         }
+
+    if(zeros || nans || infs)
+    fprintf(stream, "zeros=%d, nans=%d, infs=%d, min=%g, max=%g\n", zeros, nans, infs, min, max);
+}
+
--- src/la.h
+++ src/la.h
@@ -3,11 +3,19 @@
 
 #include <gsl/gsl_blas.h>
 
+#ifndef MIN
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#endif
+#ifndef MAX
+#define MAX(a,b) (((a)>(b))?(a):(b))
+#endif
+
 #define gsl_matrix_mult(A,B,C) (gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1, A, B, 0, C))
 #define LOGDET1M_TAYLOR_EPS 1e-12
 
 double la_norm_froebenius(gsl_matrix *M);
 double la_matrix_trace(gsl_matrix *A);
+void la_matrix_info(FILE *stream, gsl_matrix *M);
 
 
 #endif
