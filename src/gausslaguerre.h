d4fec109bba3a61cf1bf91a902ec04895e598e72
Michael Hartmann <github@speicherleck.de>
2013-12-16 08:20:01 +0100

    * adjust prototype

--- src/gausslaguerre.h
+++ src/gausslaguerre.h
@@ -1,7 +1,7 @@
 #ifndef __GAUSSLAGUERRE_H
 #define __GAUSSLAGUERRE_H
 
-double gausslaguerre_integrate(double(callback(double,void*)), void *params, int n);
+double gauss_laguerre_integrate(double(f(double,void*)), void *params, int n);
 void   gausslaguerre_integrate_vec(void(callback(double,void*,double *,int)), void *params, int n, double *vec, int len);
 
 #endif
