6eac5ace6e354fab39636ccf49f32e8c3af7f99f
Michael Hartmann <github@speicherleck.de>
2013-12-17 10:46:04 +0100

    * very simple wrapper for python

---
+++ src/Casimir.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import subprocess
+
+def PerfectReflectors(Q=0.5, T=1, lfac=5):
+    proc = subprocess.Popen(['casimir', '-Q', str(Q), '-T', str(T)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    for line in iter(proc.stdout.readline,''):
+        line = line.strip()
+        if len(line) == 0 or line[0] == '#':
+            continue
+
+        # R/(L+R), T, F, lmax, nmax, time
+        return map(float, line.split(","))
