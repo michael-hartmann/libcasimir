836e42a6edd4619952c5abbbc0e4c307e585897d
Michael Hartmann <github@speicherleck.de>
2014-01-18 12:17:15 +0100

    * log_polyintegrate: calculate log of integrals * use generic function/macro lngamma instead of gsl-function * bugfix for m==0: A,C,D should be 0, B != 0

--- src/integration.h
+++ src/integration.h
@@ -8,6 +8,7 @@ double polyintegrate(double p[], size_t len, int l1, int l2, int m, double scale
 void polym(double p[], int m, double xi);
 void polyplm(double pl1[], double pl2[], int l1, int l2, int m, double xi);
 void polydplm(double pl1[], double pl2[], int l1, int l2, int m, double xi);
-int casimir_integrate(casimir_integrals_t *cint, int l1, int l2, int m, double xi, double scale);
+double log_polyintegrate(double p[], size_t len, int l1, int l2, int m, int *sign);
+int casimir_integrate(casimir_integrals_t *cint, int l1, int l2, int m, double xi);
 
 #endif
