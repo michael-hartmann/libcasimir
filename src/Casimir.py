535aa0ffafaf6938963d41e1975959e95a9ca507
Michael Hartmann <github@speicherleck.de>
2014-01-19 17:52:20 +0100

    * add python module

---
+++ src/Casimir.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import subprocess
+
+
+def PerfectReflectors(Q=0.5, T=1, lfac=5,prec=1e-5):
+    proc = subprocess.Popen(['casimir', '-p', str(prec), '-Q', str(Q), '-T', str(T)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    for line in iter(proc.stdout.readline,''):
+        line = line.strip()
+        if len(line) == 0 or line[0] == '#':
+            continue
+
+        # R/(L+R), T, F, lmax, nmax, time
+        return map(float, line.split(","))
