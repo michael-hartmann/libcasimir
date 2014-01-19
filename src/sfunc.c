3f159802494910ef40a176c31f3da64b52aa7823
Michael Hartmann <github@speicherleck.de>
2014-01-19 15:03:05 +0100

    * add function binom * bessel functions should work, but not tested

--- src/sfunc.c
+++ src/sfunc.c
@@ -4,145 +4,79 @@
 #include "sfunc.h"
 
 
-double bessel_lnInuKnu(const int nu, const double x, double *lnInu_p, double *lnKnu_p)
+double inline binom(int n, int k)
 {
-    int k;
-    __float128 lnInu, lnKnu, lnKnup;
+    return exp(lngamma(1+n)-lngamma(1+k)-lngamma(1+n-k));
+}
 
-    // calculate Knu and Knu+
+void bessel_lnInuKnu(int nu, const double x, double *lnInu_p, double *lnKnu_p)
+{
+    int l;
+    __float128 lnKnu = 1, lnKnup = 1+1./x;
+
+    // calculate Knu, Knup
     {
         double prefactor = -x+0.5*(M_LOGPI-M_LN2-log(x));
-        __float128 Km = 1, Kp = 1+1./x;
 
         if(nu == 0)
-            return prefactor+logq(Km);
-        else if(nu == 1)
-            return prefactor+logq(Kp);
+        {
+            lnKnu  = prefactor+logq(lnKnu);
+            lnKnup = prefactor+logq(lnKnup);
+        }
         else
-            for(k = 2; k <= nu+1; k++)
+        {
+            for(l = 2; l <= nu+1; l++)
             {
-                __float128 Kn = (2*k-1)*Kp/x + Km;
-                Km = Kp;
-                Kp = Kn;
+                __float128 Kn = (2*l-1)*lnKnup/x + lnKnu;
+                lnKnu  = lnKnup;
+                lnKnup = Kn;
             }
 
-        lnKnu  = prefactor+logq(Km);
-        lnKnup = prefactor+logq(Kp);
+            lnKnup = prefactor+logq(lnKnup);
+            lnKnu  = prefactor+logq(lnKnu);
+        }
+
+        if(lnKnu_p != NULL)
+            *lnKnu_p = lnKnu;
     }
 
+    if(lnInu_p != NULL)
     {
         #define an(n,nu,x) (2*(nu+0.5+n)/x)
 
-        double nom   = an(2,nu,x)+1/an(1,nu,x);
-        double denom = an(2,nu,x);
-        double ratio = (an(1,nu,x)*nom)/denom;
-        double ratio_last = 0;
+        __float128 nom   = an(2,nu,x)+1/an(1,nu,x);
+        __float128 denom = an(2,nu,x);
+        __float128 ratio = (an(1,nu,x)*nom)/denom;
+        __float128 ratio_last = 0;
 
-        k = 3;
+        l = 3;
         while(1)
         {
-            nom   = an(k,nu,x)+1/nom;
-            denom = an(k,nu,x)+1/denom;
+            nom   = an(l,nu,x)+1/nom;
+            denom = an(l,nu,x)+1/denom;
             ratio *= nom/denom;
 
             if(ratio_last != 0 && fabs(1-ratio/ratio_last) < 1e-10)
                 break;
 
             ratio_last = ratio;
-            k++;
+            l++;
         }
 
-        return -log(x*(exp(lnKnup)+1/ratio*exp(lnKnu)));
-    }
-
-    if(lnInu_p != NULL)
-        *lnInu_p = lnInu;
-    if(lnKnu_p != NULL)
-        *lnKnu_p = lnKnu;
-}
-
-#if 1
-double bessel_lnInu(const int n, const double x)
-{
-    int k = 3;
-    const double nu = n+0.5;
-
-    #undef an
-    #define an(n,nu,x) (2*(nu+n)/x)
-
-    double nom   = an(2,nu,x)+1/an(1,nu,x);
-    double denom = an(2,nu,x);
-    double ratio = (an(1,nu,x)*nom)/denom;
-    double ratio_last = 0;
-    const double lnKnu  = bessel_lnKnu(n,x);
-    const double lnKnup = bessel_lnKnu(n+1,x);
-
-    while(1)
-    {
-        nom   = an(k,nu,x)+1/nom;
-        denom = an(k,nu,x)+1/denom;
-        ratio *= nom/denom;
-
-        if(ratio_last != 0 && fabs(1-ratio/ratio_last) < 1e-10)
-            break;
-
-        ratio_last = ratio;
-        k++;
-    }
-
-    return -log(x*(exp(lnKnup)+1/ratio*exp(lnKnu)));
-}
-#else
-double bessel_lnInu(const int n, const double x)
-{
-    double nu = n+0.5;
-
-    if(x < sqrt(nu+1)/1e4)
-        return nu*(log(x)-M_LN2)-lgamma(nu+1);
-    else if(x > nu*nu*1e4)
-        return x-0.5*log(2*M_PI*x)+log1p((4*nu*nu-1)/(8*x));
-    else
-    {
-        int k = 0;
-        const double prefactor = nu*(log(x)-M_LN2);
-        const __float128 xsqr4 = 2*(logq(x)-M_LN2q);
-        __float128 value = 0;
-
-        while(1)
-        {
-            __float128 denom = lgammaq(1+k)+lgammaq(nu+k+1);
-            __float128 term = expq(k*xsqr4-denom);
-            value += term;
-
-            if(fabsq(term/value) < 1e-25)
-                break;
-
-            k++;
-        }
-
-        return prefactor+logq(fabsq(value));
+        *lnInu_p = -log(x)-lnKnu-logq(expq(lnKnup-lnKnu)+1/ratio);
     }
 }
-#endif
 
 double bessel_lnKnu(const int nu, const double x)
 {
-    int l;
-    double prefactor = -x+0.5*(M_LOGPI-M_LN2-log(x));
-    __float128 Km = 1;
-    __float128 Kp = 1+1./x;
+    double Knu;
+    bessel_lnInuKnu(nu, x, NULL, &Knu);
+    return Knu;
+}
 
-    if(nu == 0)
-        return prefactor+logq(Km);
-    else if(nu == 1)
-        return prefactor+logq(Kp);
-
-    for(l = 2; l <= nu; l++)
-    {
-        __float128 Kn = (2*l-1)*Kp/x + Km;
-        Km = Kp;
-        Kp = Kn;
-    }
-
-    return prefactor+logq(Kp);
+double bessel_lnInu(const int nu, const double x)
+{
+    double Inu;
+    bessel_lnInuKnu(nu, x, &Inu, NULL);
+    return Inu;
 }
--- src/sfunc.h
+++ src/sfunc.h
@@ -8,14 +8,10 @@
 #define pow_2(x) (x*x)
 #define lnfac(x) (lgamma(1+x))
 
-#include <gsl/gsl_sf.h>
+double inline binom(int n, int k);
 
-/*
-#define bessel_lnInu(n,x) (log(gsl_sf_bessel_Inu(n+0.5,x)))
-#define bessel_lnKnu(n,x) (gsl_sf_bessel_lnKnu(n+0.5,x))
-*/
 double bessel_lnInu(const int n, const double x);
 double bessel_lnKnu(const int n, const double x);
-double bessel_lnInuKnu(const int nu, const double x, double *lnInu, double *lnKnu);
+void bessel_lnInuKnu(const int nu, const double x, double *lnInu, double *lnKnu);
 
 #endif
