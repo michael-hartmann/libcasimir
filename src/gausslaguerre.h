03d3721154991ecc62a104faf90fd388feebe825
Michael Hartmann <github@speicherleck.de>
2014-01-19 15:05:12 +0100

    we don't need gauss-laguerre integration anymore

--- src/gausslaguerre.h
+++
@@ -1,7 +0,0 @@
-#ifndef __GAUSSLAGUERRE_H
-#define __GAUSSLAGUERRE_H
-
-double gauss_laguerre_integrate(double(f(double,void*)), void *params);
-void   gausslaguerre_integrate_vec(void(callback(double,void*,double *,int)), void *params, double *vec, int len);
-
-#endif
