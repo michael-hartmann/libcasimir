ae98268252cf15881ded4f7d61405677dd9ee237
Michael Hartmann <github@speicherleck.de>
2014-01-25 12:17:28 +0100

    * fix error

--- src/quad.h
+++ src/quad.h
@@ -4,16 +4,20 @@
 #if defined(__ICC) || defined(__INTEL_COMPILER)
     #define __float128 _Quad
 
+    /* deine isinf and isnan */
     #define isinfq(x) (x/10 == x)
     #define isnanq(x) (x != x)
 
+    /* define prototypes. without these prototypes icc will return nan. */
     _Quad __logq(_Quad);
     _Quad __expq(_Quad);
     _Quad __sqrtq(_Quad);
     _Quad __log1pq(_Quad);
 
     #define logq __logq
-    #define expq __logq
+    #define expq __expq
+    #define sqrtq __sqrtq(_Quad);
+    #define log1pq __log1pq(_Quad);
 #elif defined(__GNUC__) || defined(__GNUG__)
     #include <quadmath.h>
 #endif
