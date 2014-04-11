f12435770773f4c90809ac4b20448370d3cde77c
Michael Hartmann <github@speicherleck.de>
2014-04-11 10:31:58 +0200

    * add preprocessor defines

--- src/edouble.h
+++ src/edouble.h
@@ -17,7 +17,7 @@
     #define isnanq(x) (x != x)
 #else
     #if defined(__ICC) || defined(__INTEL_COMPILER)
-        #define CASIMIR_ARITHMETICS "icc quad"
+        #define CASIMIR_ARITHMETICS "icc _Quad"
         #define edouble _Quad
     
         /* define isinf and isnan */
@@ -50,13 +50,33 @@
         #define copysignq __copysignq
     
     #elif defined(__GNUC__) || defined(__GNUG__)
-        #define CASIMIR_ARITHMETICS "gcc quad"
+        #define CASIMIR_ARITHMETICS "gcc __float128"
 
         #include <quadmath.h>
         #define edouble __float128
     #else
         #error "I'm sorry, but quad precision is only supported with gcc or icc at the moment."
     #endif
+#endif
+
+#if defined(__ICC) || defined(__INTEL_COMPILER)
+    #define COMPILER "icc"
+#elif defined(__GNUC__) || defined(__GNUG__)
+    #define COMPILER "gcc"
+#else
+    #define COMPILER "unknown"
+#endif
+
+#ifdef MATRIX_QUAD
+    #define CASIMIR_MATRIX "plain"
+#else
+    #define CASIMIR_MATRIX "log"
+#endif
+
+#ifdef INTEGRATION_QUAD
+    #define CASIMIR_INTEGRATION "plain"
+#else
+    #define CASIMIR_INTEGRATION "log"
 #endif
 
 #endif
