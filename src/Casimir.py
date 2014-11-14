423a6afdab7bdbe00ff9670284c5af44b83eeaea
Michael Hartmann <github@speicherleck.de>
2014-11-14 18:21:21 +0100

    * adjust Casimir.py

--- src/Casimir.py
+++ src/Casimir.py
@@ -4,7 +4,8 @@ import subprocess
 
 
 def PerfectReflectors(Q=0.5, T=1, cores=1, lfac=5,prec=1e-5):
-    proc = subprocess.Popen(['casimir', '-c', str(cores), '-l', str(lfac), '-p', str(prec), '-Q', str(Q), '-T', str(T)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    x = 1./Q-1
+    proc = subprocess.Popen(['casimir', '-c', str(cores), '-l', str(lfac), '-p', str(prec), '-x', str(x), '-T', str(T)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     for line in iter(proc.stdout.readline,''):
         line = line.strip()
         if len(line) == 0 or line[0] == '#':
