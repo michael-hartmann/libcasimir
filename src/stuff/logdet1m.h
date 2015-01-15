df4168309ed49caeaf6ff5accc713ae968548bf7
Michael Hartmann <github@speicherleck.de>
2015-01-15 23:46:46 +0100

    remove old files

--- src/stuff/logdet1m.h
+++
@@ -1,8 +0,0 @@
-#ifndef __LOGDET1M_H
-#define __LOGDET1M_H
-
-#include <gsl/gsl_matrix.h>
-
-int logdet1m_eigenvalues(gsl_matrix *M, double *logdet);
-
-#endif
--- src/stuff/poly.h
+++
@@ -1,8 +0,0 @@
-#ifndef __POLY_H
-#define __POLY_H
-
-void poly_multiply_naive(edouble p1[], size_t len_p1, edouble p2[], size_t len_p2, edouble pdest[]);
-void poly_add(edouble p1[], size_t len_p1, edouble p2[], size_t len_p2, edouble pdest[]);
-void poly_mult_karatsuba(edouble p1[], size_t len_p1, edouble p2[], size_t len_p2, edouble pdest[]);
-
-#endif
