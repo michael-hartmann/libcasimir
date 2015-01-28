5cc1cca098c1d3fe6db1223a8bd656805dcfcdb3
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2015-01-28 17:43:52 +0100

    fix signs

--- src/tests/test_integration.py
+++ src/tests/test_integration.py
@@ -9,66 +9,69 @@ from test_fresnel import fresnel_rp
 
 mp.dps = 5
 
-
-def rp(nT, omegap, gamma, p):
-    if p.lower() == "te":
-        return lambda k: -1
-        #return lambda k: fresnel_rp(nT, k, omegap, gamma_)[0]
-    else:
-        return lambda k: +1
-        #return lambda k: fresnel_rp(nT, k, omegap, gamma_)[1]
-
-
-def integralA(l1,l2,m,nT, omegap, gamma_, p="te"):
+def integralA(l1,l2,m,nT, rp):
     tau = 2*nT
-    r = rp(nT, omegap, gamma, p)
 
     def integrandA(x):
         k = sqrt(x**2/4 + nT*x)
-        return r(k)*exp(-x)/(x**2+2*tau*x)*Plm(l1,m,1+x/tau)*Plm(l2,m,1+x/tau)
+        return rp(nT,k)*exp(-x)/(x**2+2*tau*x)*Plm(l1,m,1+x/tau)*Plm(l2,m,1+x/tau)
 
-    result = (-1)**(l2+m)*m**2*tau*exp(-tau)*exp(lnLambda(l1,l2,m))*quad(integrandA, [0, inf])
+    # the signs are non trivial: the factor (-1)**(m%2) arises because
+    # Plm(l1,m,x)*Plm(l2,m,x) gives a factor of i**2 for m odd.
+    # Lambda(l1,l2,m) = -exp(lnLambda(l1,l2,m))
+    result = (-1)**(m%2+l2+m)*m**2*tau*exp(-tau)*-exp(lnLambda(l1,l2,m))*quad(integrandA, [0, inf])
     return log(abs(result)), int(sign(result))
 
 
-def integralB(l1,l2,m,nT, omegap, gamma_, p="te"):
+def integralB(l1,l2,m,nT, rp):
     tau = 2*nT
-    r = rp(nT, omegap, gamma, p)
 
     def integrandB(x):
         k = sqrt(x**2/4 + nT*x)
-        return r(k)*exp(-x)*(x**2+2*tau*x)*dPlm(l1,m,1+x/tau)*dPlm(l2,m,1+x/tau)
+        return rp(nT,k)*exp(-x)*(x**2+2*tau*x)*dPlm(l1,m,1+x/tau)*dPlm(l2,m,1+x/tau)
 
-    result = (-1)**(l2+m+1)*exp(-tau)/tau**3*exp(lnLambda(l1,l2,m))*quad(integrandB, [0, inf])
+    result = (-1)**(m%2+l2+m+1)*exp(-tau)/tau**3*-exp(lnLambda(l1,l2,m))*quad(integrandB, [0, inf])
     return log(abs(result)), int(sign(result))
 
 
-def integralC(l1,l2,m,nT, omegap, gamma_, p="te"):
+def integralC(l1,l2,m,nT, rp):
     tau = 2*nT
-    r = rp(nT, omegap, gamma, p)
 
     def integrandC(x):
         k = sqrt(x**2/4 + nT*x)
-        return r(k)*exp(-x)*Plm(l1,m,1+x/tau)*dPlm(l2,m,1+x/tau)
+        return rp(nT,k)*exp(-x)*Plm(l1,m,1+x/tau)*dPlm(l2,m,1+x/tau)
 
-    result = (-1)**(l2+m)*m*exp(-tau)/tau*exp(lnLambda(l1,l2,m))*quad(integrandC, [0, inf])
+    result = (-1)**(m%2+l2+m)*m*exp(-tau)/tau*-exp(lnLambda(l1,l2,m))*quad(integrandC, [0, inf])
     return log(abs(result)), int(sign(result))
 
 
-def integralD(l1,l2,m,nT, omegap, gamma_, p="te"):
+def integralD(l1,l2,m,nT, rp):
     tau = 2*nT
-    r = rp(nT, omegap, gamma, p)
 
     def integrandD(x):
         k = sqrt(x**2/4 + nT*x)
-        return r(k)*exp(-x)*dPlm(l1,m,1+x/tau)*Plm(l2,m,1+x/tau)
+        return rp(nT,k)*exp(-x)*dPlm(l1,m,1+x/tau)*Plm(l2,m,1+x/tau)
 
-    result = (-1)**(l2+m+1)*m*exp(-tau)/tau*exp(lnLambda(l1,l2,m))*quad(integrandD, [0, inf])
+    result = (-1)**(m%2+l2+m+1)*m*exp(-tau)/tau*-exp(lnLambda(l1,l2,m))*quad(integrandD, [0, inf])
     return log(abs(result)), int(sign(result))
 
 
 if __name__ == "__main__":
     omegap = 1.32e2
     gamma_ = 6.9e-1
+    r_TE_drude = lambda nT,k: fresnel_rp(nT, k, omegap, gamma_)[0]
+    r_TM_drude = lambda nT,k: fresnel_rp(nT, k, omegap, gamma_)[1]
+    r_TE_perf  = lambda nT,k: -1
+    r_TM_perf  = lambda nT,k: +1
 
-    print integralA(3, 2, 1, 2, omegap, gamma_, p="tm")
+    print integralA(3, 2, 2, 1, r_TE_drude)
+    print integralA(3, 2, 2, 1, r_TM_drude)
+
+    print integralB(3, 2, 2, 1, r_TE_drude)
+    print integralB(3, 2, 2, 1, r_TM_drude)
+
+    print integralC(3, 2, 2, 1, r_TE_drude)
+    print integralC(3, 2, 2, 1, r_TM_drude)
+
+    print integralD(3, 2, 2, 1, r_TE_drude)
+    print integralD(3, 2, 2, 1, r_TM_drude)
